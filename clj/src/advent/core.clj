69bfa042e4d014e08a2b4f40d177891f13cfb6ec
Auclair Emmanuel <auclair.emmanuel@gmail.com>
2021-01-01 17:26:58 +0100

    Clj day 1

--- clj/src/advent/core.clj
+++ clj/src/advent/core.clj
@@ -1,6 +1,29 @@
-(ns advent.core)
+(ns advent.core
+  (:require [clojure.java.io :as io]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn day1-find-mult
+  [target-sum inputs]
+  (loop [[x & xs] inputs]
+    (when-not (nil? x)
+      (let [y (first (filter #(= target-sum (+ x %)) xs))]
+        (if-not (nil? y)
+          (* x y)
+          (recur xs))))))
+
+(comment
+  (with-open [day1 (io/reader "/home/auclair/code/perso/advent2020/day1/input.txt")]
+    (def day1-input (into [] (map #(Integer/parseInt %) (line-seq day1) ))))
+
+  (day1-find-mult 2020 day1-input)
+  ;; => 1020099
+
+  (count day1-input)
+
+  (loop [[x & xs] day1-input]
+    (when-not (nil? x)
+      (let [mult (day1-find-mult (- 2020 x) xs)]
+        (if-not (nil? mult)
+          (* x mult)
+          (recur xs)))))
+  ;; => 9214880
+  )
